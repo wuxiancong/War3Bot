52cb6315dbe70e4dc305e9a3ab3d0ac0134ac746
Wuxiancong <48830399+wuxiancong@users.noreply.github.com>
2026-01-17 19:14:29 +0800

    ...

--- src/client.cpp
+++ src/client.cpp
@@ -2966,9 +2966,9 @@ void Client::initSlots(quint8 maxPlayers)
     m_playerBuffers.clear();
 
     static const quint8 DOTA_COLORS[] = {
-        0, 1, 2, 3, 4,
-        5, 6, 7, 8, 9,
-        10, 11
+        1, 2, 3, 4, 5,
+        7, 8, 9, 10, 11,
+        0, 6
     };
 
     // 初始化地图槽
